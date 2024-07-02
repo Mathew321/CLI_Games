a8998a8806f2d48fd826f794f7ec7496eeb4a369
Mathew321 <61094867+Mathew321@users.noreply.github.com>
2024-07-02 12:27:55 +0200

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,56 +1,66 @@
-import os, random
+import os, random, json
 from colorama import Fore, init
 
 class Wordle():
     def __init__(self):
         self.clear = lambda: os.system("clear")
         self.words = [
-            "about", "after", "again", "below", "could", "every", "first", "found", "great", "house",
-            "large", "never", "other", "place", "point", "right", "small", "sound", "still", "table",
-            "these", "thing", "think", "three", "water", "where", "which", "world", "would", "years",
-            "apple", "beach", "birth", "black", "brown", "carry", "chain", "clean", "clear", "close",
-            "dance", "dream", "drink", "earth", "eight", "field", "fight", "floor", "force", "forth",
-            "fresh", "front", "glass", "green", "happy", "heart", "horse", "human", "image", "large",
-            "learn", "light", "magic", "metal", "music", "north", "ocean", "order", "paper", "party",
-            "plant", "press", "queen", "quick", "reach", "river", "round", "sense", "seven", "shall",
-            "shape", "share", "sharp", "sheet", "shift", "shoot", "short", "since", "skill", "sleep",
-            "smile", "south", "space", "speak", "speed", "spend", "sport", "staff", "stage", "stand",
-            "start", "state", "steam", "stone", "story", "table", "taste", "teach", "thank", "their",
-            "there", "these", "thick", "thing", "think", "third", "three", "throw", "tiger", "title",
-            "today", "topic", "total", "touch", "trade", "train", "treat", "trend", "trust", "truth",
-            "twice", "under", "until", "upper", "usual", "valid", "value", "video", "visit", "voice",
-            "watch", "water", "where", "which", "whole", "world", "worry", "worse", "worth", "would",
-            "write", "wrong", "years", "young", "youth"]
+            "about", "after", "again", "below", "could", "every", "first", "found", "great",
+            "house", "large", "never", "other", "place", "point", "right", "small", "sound",
+            "still", "table", "these", "thing", "think", "three", "water", "where", "which",
+            "world", "would", "years", "apple", "beach", "birth", "black", "brown", "carry",
+            "chain", "clean", "clear", "close", "dance", "dream", "drink", "earth", "eight",
+            "field", "fight", "floor", "force", "forth", "fresh", "front", "glass", "green",
+            "happy", "heart", "horse", "human", "image", "large", "learn", "light", "magic",
+            "metal", "music", "north", "ocean", "order", "paper", "party", "plant", "press",
+            "queen", "quick", "reach", "river", "round", "sense", "seven", "shall", "shape",
+            "share", "sharp", "sheet", "shift", "shoot", "short", "since", "skill", "sleep",
+            "smile", "south", "space", "speak", "speed", "spend", "sport", "staff", "stage",
+            "stand", "start", "state", "steam", "stone", "story", "table", "taste", "teach",
+            "thank", "their", "there", "these", "thick", "thing", "think", "third", "three",
+            "throw", "tiger", "title", "today", "topic", "total", "touch", "trade", "train",
+            "treat", "trend", "trust", "truth", "twice", "under", "until", "upper", "usual",
+            "valid", "value", "video", "visit", "voice", "watch", "water", "where", "which",
+            "whole", "world", "worry", "worse", "worth", "would", "write", "wrong", "years",
+            "young", "youth"]
         self.word = self.words[random.randrange(0,len(self.words))]
         self.triedWords = ["_ _ _ _ _ " for i in range(0,5)]
         self.guessed = False
+
+    def game(self, loop):
+        self.guess = input("Word: ")
+        self.guessedWord = ""
+        self.allWords = []
+        with open("file.json","r") as file:
+            self.allWords = json.load(file)['words']
+
+        if len(self.guess) == 5 and self.guess in self.allWords:
+            for i in range(0,len(self.guess)):
+                if self.guess[i] in self.word:
+                    if self.guess[i] == self.word[i]:
+                        # Letter should be green if letter in word and in right pos
+                        self.guessedWord += Fore.GREEN + self.guess[i] + " "
+                    else:
+                        # Letter should be yellow if letter is in word but not in the right pos
+                        self.guessedWord += Fore.YELLOW + self.guess[i] + " "
+                else:
+                    # Letter should be grey if letter not in word
+                    self.guessedWord += "\033[90m" + self.guess[i] + " "
+            self.triedWords[loop] = self.guessedWord
+            if self.guess == self.word:
+                self.guessed = True
+                return True
+        else:
+            print("\nInvalid word!\nThe word should be 5 characters long!\nAnd has to exist!\n")
+            self.game(loop)
+        return False
 
     def startup(self):
         for loop in range(0,5):
             self.clear()
             [print(word) for word in self.triedWords]
-            self.guess = input("Word: ")
-            self.guessedWord = ""
-
-            if len(self.guess) == 5:
-                for i in range(0,len(self.guess)):
-                    if self.guess[i] in self.word:
-                        if self.guess[i] == self.word[i]:
-                            # Letter should be green
-                            self.guessedWord += Fore.GREEN + self.guess[i] + " "
-                        else:
-                            # Letter should be yellow
-                            self.guessedWord += Fore.YELLOW + self.guess[i] + " "
-                    else:
-                        # Letter should be red
-                        self.guessedWord += "\033[90m" + self.guess[i] + " "
-
-                self.triedWords[loop] = self.guessedWord
-                if self.guess == self.word:
-                    self.guessed = True
-                    break
-            else:
-                print("\nInvalid word!\nThe word should be 5 characters long!\n")
+            if self.game(loop):
+                break
 
     def replay(self):
         r = input("Would you like to play again? ")
